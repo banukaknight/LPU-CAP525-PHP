ad27c3b4c4f7bc7706f148bceaa3dbda38742489
Banuka <banukaknight@gmail.com>
2020-10-24 21:33:43 +0530

    thebkrox

--- T00_PHP_additionals/d201023_ip_logging.php
+++ T00_PHP_additionals/d201023_ip_logging.php
@@ -16,8 +16,9 @@ if ($conn->connect_error) {
 if (! $conn->query ("DESCRIBE $tbname1" )) {
 	$sql = "CREATE TABLE iplogs (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	v_ip VARCHAR(30) NOT NULL,
-	v_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	v_ip VARCHAR(30) UNIQUE NOT NULL,
+	v_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	v_count int(6) NOT NULL
 	)";
 
 	if ($conn->query($sql) === TRUE) {
@@ -29,7 +30,11 @@ if (! $conn->query ("DESCRIBE $tbname1" )) {
 
 $ip = getenv("REMOTE_ADDR"); //get ip address of visitor
 
-$sql = "INSERT INTO $tbname1 (v_ip) VALUES ('$ip')";
+$sql = "
+INSERT INTO $tbname1 (v_ip, v_count) 
+VALUES ('$ip', 1)
+ON DUPLICATE KEY UPDATE ;
+";
 if ($conn->query($sql) === TRUE) {
 	  echo("New record created successfully");
 	} else {
@@ -37,21 +42,32 @@ if ($conn->query($sql) === TRUE) {
 	}
 
 
-//results
-$sql = "SELECT * FROM $tbname1";
-$result = $conn->query($sql);
+if(isset($_POST['password'])){
+	if($_POST['password']==="Banuka"){
+		//results
+		$sql = "SELECT * FROM $tbname1";
+		$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-	echo "<table border='1'>";
-		echo "<tr><th>ID</th><th>IP</th><th>Date-Time</th></tr>";
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["v_ip"]. "</td><td>" . $row["v_date"]. "</td></tr>";
-  }
-  echo "</table>";
-} else {
-  echo "<h4>No records found in table.</h4>";
+		if ($result->num_rows > 0) {
+			echo "<table border='1'>";
+				echo "<tr><th>ID</th><th>IP</th><th>Date-Time</th><th>Count</th></tr>";
+		  while($row = $result->fetch_assoc()) {
+		    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["v_ip"]. "</td><td>" . $row["v_date"].  "</td><td>" . $row["v_count"]. "</td></tr>";
+		  }
+		  echo "</table>";
+		} else {
+		  echo "<h4>No records found in table.</h4>";
+		}
+	}else{
+		echo "<h4>Nothing to see here buddy<h4>";
+	}
 }
+
+
 
 ?>
 
 <h1>Hello Stranger</h1>
+
+<form method="POST">PW: <input type="text" name="password">
+<input type="submit" name=""></form>
